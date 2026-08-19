16e24f027f8f814b62bf358f0e7ea0e1d9c57201
Lívia Borges Matos <livia.matos2721@gmail.com>
2026-08-19 13:27:58 -0300

    feat: criação da função produtoPorCodigo

--- src/controller/ProdutoController.ts
+++ src/controller/ProdutoController.ts
@@ -46,6 +46,31 @@ class ProdutoController {
         }
     }
 
+     /*  Busca produto pelo Código Único 
+     */
+    static async produtoPorCodigo(req: Request, res: Response): Promise<Response> {
+        try {
+            const codigo = req.params.codigo as string;
+
+            if (!codigo) {
+                return res.status(400).json({ mensagem: "É necessário informar o código do produto." });
+            }
+
+            const produto: ProdutoDTO | null = await Produto.buscarPorCodigo(codigo);
+
+            if (produto !== null) {
+                return res.status(200).json(produto);
+            } else {
+                return res.status(404).json({ mensagem: "Nenhum produto foi encontrado com o código fornecido." });
+            }
+        } catch (error) {
+            console.error(`Erro ao buscar produto por código: ${error}`);
+            return res.status(500).json({ mensagem: "Erro interno no servidor." });
+        }
+    }
+
+
+
 
 }
 export default ProdutoController;
